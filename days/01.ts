b54800578aed814f759c0e8265fc13538c958b42
Gio Wolpert <giow@delta-n.nl>
2021-12-01 13:56:41 +0100

    Fixed template to fetch input dynamically

--- days/01.ts
+++ days/01.ts
@@ -2,7 +2,7 @@ export const p1 = (input: string): number => countIncreases(input.split('\n').ma
 
 export const p2 = (input: string): number => {
   const lines = input.split('\n').map(Number);
-  return countIncreases(lines.map((x, i) =>  x + lines[i + 1] + lines[i + 2]));
+  return countIncreases(lines.map((x: number, i: number) =>  x + lines[i + 1] + lines[i + 2]));
 }
 
 const countIncreases = (lines: number[]): number => {
--- index.ts
+++ index.ts
@@ -1,13 +1,18 @@
 import { ensureDir } from "https://deno.land/std@0.116.0/fs/ensure_dir.ts";
+import { config } from 'https://deno.land/x/dotenv@v3.1.0/mod.ts';
 import { brightYellow, underline, brightBlue } from 'https://deno.land/std@0.116.0/fmt/colors.ts';
 
 const [ cmd, dayNumber, partNumber ] = Deno.args;
+if (!dayNumber.match(/^\d{1,2}$/)) throw new Error('Day number provided is incorrect')
 const day = String(+dayNumber).padStart(2, '0');
+const env = config();
 
 switch(cmd) {
   case 'run': {
-    const file = await import(`./day${day}/index.ts`);
-    const input = (await Deno.readTextFile(`./day${day}/input.txt`) || '');
+    const file = await import(`./days/${day}.ts`);
+    const response = await fetch(`https://adventofcode.com/2021/day/${dayNumber}/input`, { headers: { cookie: `session=${env.ADVENT_SESSION_TOKEN}` }});
+    if (!response.ok) throw new Error('Advent of Code Session Token not set');
+    const input = await response.text();
     const runningDay = `${brightBlue('Running day')} ${brightYellow(dayNumber)} ${brightBlue('part')}`;
     if (!partNumber || +partNumber === 1) {
       const timerStart = performance.now();
@@ -27,10 +32,9 @@ switch(cmd) {
   }
   case 'create': {
     console.log(`Creating day ${dayNumber}`)
-    const template = await Deno.readTextFile('./template.ts.tmpl');
-    await ensureDir(`./day${day}`);
-    await Deno.writeTextFile(`./day${day}/index.ts`, template);
-    await Deno.writeTextFile(`./day${day}/input.txt`, '');
+    const template = await Deno.readTextFile('./template.ts');
+    await ensureDir(`./days`);
+    await Deno.writeTextFile(`./days/${day}.ts`, template);
     break;
   }
   default: {
